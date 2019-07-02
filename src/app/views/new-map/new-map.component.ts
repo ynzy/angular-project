14aee99fd7c9451dc146532924046ca0582ddc80
zhangyong <zhangyong@163.com>
2019-07-02 17:44:05 +0800

    我的大陆静态页面

--- src/app/views/new-map/new-map.component.ts
+++ src/app/views/new-map/new-map.component.ts
@@ -6,7 +6,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-map.component.scss']
 })
 export class NewMapComponent implements OnInit {
-
+  list:any[] = [
+    {
+      name: '北京',
+      desc: '北京好，风景真的不错！',
+      price: '2000',
+      date: '2018-12-29',
+      hasDone: true,
+      location: {
+        lat: 39.910924,
+        lng: 116.413387
+      }
+    },
+    {
+      name: '苏州',
+      desc: '苏州好，去了还想去，不错！',
+      price: '2000',
+      hasDone: true,
+      date: '2018-12-29',
+      location: {
+        lat: 31.303565,
+        lng: 120.592412
+      }
+    },{
+      name: '北京',
+      desc: '北京好，风景真的不错！',
+      price: '2000',
+      date: '2018-12-29',
+      hasDone: true,
+      location: {
+        lat: 39.910924,
+        lng: 116.413387
+      }
+    },
+    {
+      name: '苏州',
+      desc: '苏州好，去了还想去，不错！',
+      price: '2000',
+      hasDone: true,
+      date: '2018-12-29',
+      location: {
+        lat: 31.303565,
+        lng: 120.592412
+      }
+    },{
+      name: '北京',
+      desc: '北京好，风景真的不错！',
+      price: '2000',
+      date: '2018-12-29',
+      hasDone: false,
+      location: {
+        lat: 39.910924,
+        lng: 116.413387
+      }
+    },
+    {
+      name: '苏州',
+      desc: '苏州好，去了还想去，不错！',
+      price: '2000',
+      hasDone: false,
+      date: '2018-12-29',
+      location: {
+        lat: 31.303565,
+        lng: 120.592412
+      }
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
